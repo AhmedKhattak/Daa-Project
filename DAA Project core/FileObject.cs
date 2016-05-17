855540eec34251b11c41e396be2a8bdf92df748b
Ahmed Khattak <khattak.ahmed@yahoo.com>
2016-05-17 07:50:21 -0700

    Removed  nonessential usings

--- DAA Project core/FileObject.cs
+++ DAA Project core/FileObject.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DAA_Project_core
+﻿namespace DAA_Project_core
 {
     class FileObject
     {
